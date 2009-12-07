39aa07e308977d6f5144b1fadd8e64996656d85f
wadealer <wadealer@cd52e80e-b6f7-11de-b71e-ab0a53e69959>
2009-12-07 08:04:34 +0000

    update to 0.1.8

--- main.h
+++ main.h
@@ -11,7 +11,7 @@ int ProxyPort = 3128;
 int BotPort = 5222;
 QVector<QString> BotFolders;
 QTextCodec *codec;
-QString Separator = "/";                    //Помеять тут, чтобы получить
-QString Prefix = "ftp://2.1.1.68";              //необходимые пути
+QString Separator = "/";
+QString Prefix = "Server";
 
 #endif // MAIN_H
